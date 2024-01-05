587448da7ee5dcb7606e3e9624303695d1fff6a8
CypherDOM <wbzero0@gmail.com>
2024-01-05 16:54:20 +0800

    Add a playRound() for a round of game of Rock-Paper-Scissors including the game mechanics of win-lose.

--- myMechanics.js
+++ myMechanics.js
@@ -1,7 +1,34 @@
 const getComputerChoice = () => {
-    const choice = ['rock','paper','scissors'];
+    const choice = ['Rock','Paper','Scissors'];
     
     return choice[Math.floor(Math.random()* choice.length)];
 }
 
-console.log(getComputerChoice());
+
+const playRound = (playerSelection, computerSelection) => {
+    let player = playerSelection;
+    let computer = computerSelection;
+
+    if(player === computer){
+        return 'Tied';
+    }
+    else if
+    (player === 'Rock' && computer === 'Scissors' || 
+     player === 'Paper' && computer === 'Rock' ||
+     player === 'Scissors' && computer === 'Paper'){
+
+        return 'Player';
+     }
+     else if
+    (computer === 'Rock' && player === 'Scissors' || 
+    computer === 'Paper' && player === 'Rock' ||
+    computer === 'Scissors' && player === 'Paper'){
+
+        return 'Computer';
+     }
+    
+}
+
+let playerSelection = 'Rock';
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
