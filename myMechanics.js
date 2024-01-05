58d61af4b16db50f3921db1de72f3d602343f9dc
CypherDOM <wbzero0@gmail.com>
2024-01-05 17:29:58 +0800

    added a game() that will loop the playRound until the player or computer reach the game set

--- myMechanics.js
+++ myMechanics.js
@@ -1,26 +1,29 @@
 const getComputerChoice = () => {
-    const choice = ['Rock','Paper','Scissors'];
+
+    const choice = ['Rock', 'Paper', 'Scissors'];
     
     return choice[Math.floor(Math.random()* choice.length)];
 }
 
-const formatPlayerSelection = (inputString) => {
+const formatPlayerSelection = ( inputString ) => {
     
     return inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
-
 }
 
 const resetRound = () => {
+
     let player = null;
     let computer = null;
 }
 
-const playRound = (playerSelection, computerSelection) => {
-    let player = playerSelection;
+const playRound = ( playerSelection, computerSelection ) => {
+
+    let player = formatPlayerSelection( playerSelection );
     let computer = computerSelection;
 
     if(player === computer){
         resetRound();
+
         return 'Tied';
     }
     else if
@@ -40,7 +43,37 @@ const playRound = (playerSelection, computerSelection) => {
     
 }
 
-let playerInput = "rOCk";
-let playerSelection = formatPlayerSelection(playerInput);
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+const game = () => {
+
+    let playerScore = 0;
+    let computerScore = 0;
+    const gameSet = 5;
+
+    for(let round = 1; playerScore < 5 && computerScore < 5; round++){
+        let playerSelection = prompt('Enter you move: [Rock, Paper and Scissors]');
+        let computerSelection = getComputerChoice();
+
+        const result = playRound( playerSelection, computerSelection );
+
+        if( result === 'Player' ){
+
+            playerScore++;
+            console.log( `Round ${round}: You win! ${playerSelection} beats ${computerSelection}` );
+            console.log( `Score [ Player: ${playerScore} Computer: ${computerScore} ]` );
+        }
+        else if ( result === 'Computer'){
+
+            computerScore++;
+            console.log( `Round ${round}: You lose! ${computerSelection} beats ${playerSelection}` );
+            console.log( `Score [ Player: ${playerScore} Computer: ${computerScore} ]` );
+        }
+        else {
+            console.log( `Round ${round}: Oops! Draw? Fight again!`);
+            console.log( `Score [ Player: ${playerScore} Computer: ${computerScore} ]` );
+        }
+    }
+    return (playerScore === gameSet) ? ` GAME OVER! Player wins! ` : ` GAME OVER! Computer wins! `;
+}
+
+
+console.log(game());
