3f618a822263749193222314798e9f7e0392797c
CypherDOM <wbzero0@gmail.com>
2024-01-05 16:36:30 +0800

    Add a getComputerChoice() function to generate computer moves at random

---
+++ myMechanics.js
@@ -0,0 +1,7 @@
+const getComputerChoice = () => {
+    const choice = ['rock','paper','scissors'];
+    
+    return choice[Math.floor(Math.random()* choice.length)];
+}
+
+console.log(getComputerChoice());
